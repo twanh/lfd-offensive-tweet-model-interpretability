1bd28940c73c1465b86b1dbf1d33589143b4d4a4
Twan Huiskens <huiskenstwan@gmail.com>
2025-10-28 12:29:27 +0100

    Add GPU

--- lm/run_gridsearch.sh
+++ lm/run_gridsearch.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=/scratch/%u/lfd_final/grid_search/slurm_logs/slurm-%A_%a.out
 #SBATCH --error=/scratch/%u/lfd_final/grid_search/slurm_logs/slurm-%A_%a.err
 #SBATCH --partition=gpu
-#SBATCH --gpus=1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 #SBATCH --time=04:00:00
 #SBATCH --array=0-71
 
