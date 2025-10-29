38e7f6b90db40f1ca1c076cc488fb241bd11b5bf
Twan Huiskens <huiskenstwan@gmail.com>
2025-10-29 13:00:58 +0100

    Add scores

--- lm/interpret.py
+++ lm/interpret.py
@@ -1,9 +1,11 @@
 import argparse
+from collections import defaultdict
 
 import torch
 from BERT_explainability.modules.BERT.BertForSequenceClassification import BertForSequenceClassification
 from BERT_explainability.modules.BERT.ExplanationGenerator import Generator
 from captum.attr import visualization
+from tqdm import tqdm
 from transformers import AutoTokenizer
 from transformers import BertTokenizer
 
@@ -43,6 +45,13 @@ def create_arg_parser() -> argparse.Namespace:
         default=10,
     )
 
+    parser.add_argument(
+        '--min-count',
+        help='Minimum count for a feature to be considered',
+        type=int,
+        default=10,
+    )
+
     return parser.parse_args()
 
 
@@ -74,44 +83,107 @@ def main() -> int:
     model.eval()
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_path)
-
     explainer = Generator(model)
 
     classications = ['OFF', 'NOT']
+    off_indx = classications.index('OFF')
+    not_indx = classications.index('NOT')
 
     tweets, labels = read_corpus(args.input_file)
 
-    for tweet, label in zip(tweets, labels):
+    global_off_scores = defaultdict(float)
+    global_off_counts = defaultdict(int)
 
-        true_class = classications.index(label)
+    global_not_scores = defaultdict(float)
+    global_not_counts = defaultdict(int)
+
+    special_tokens = [
+        tokenizer.cls_token,
+        tokenizer.sep_token, tokenizer.pad_token,
+    ]
+
+    for tweet in tqdm(tweets, unit='tweet'):
 
         encoding = tokenizer(tweet, return_tensors='pt')
         input_ids = encoding['input_ids'].to('cuda')
         attention_mask = encoding['attention_mask'].to('cuda')
 
-        expl = explainer.generate_LRP(
+        # Get scores for OFF class
+        expl_off = explainer.generate_LRP(
             input_ids=input_ids,
             attention_mask=attention_mask,
+            index=off_indx,
             start_layer=0,
         )[0]
-        # Normalize scores
-        expl = (expl - expl.min()) / (expl.max() - expl.min())
 
-        # Get the model classifcation
-        output = torch.nn.functional.softmax(
-            model(input_ids=input_ids, attention_mask=attention_mask)[0],
-            dim=-1,
-        )
-        predicted_class = torch.argmax(output, dim=-1).item()
+        # Get scores for NOT class
+        expl_not = explainer.generate_LRP(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            index=not_indx,
+            start_layer=0,
+        )[0]
 
-        class_name = classications[predicted_class]
-        tokens = tokenizer.convert_ids_to_tokens(
-            input_ids.flatten(),
-        )
-        print(f'Tweet: {tweet}')
-        print(f'True class: {label}, Predicted class: {class_name}')
-        print('Top explanations:')
-        print([(tokens[i], expl[i].item()) for i in range(len(tokens))])
+        # Add scores to global counts
+        for i, token in enumerate(tokens):
+            # Skip special tokens
+            if token in special_tokens:
+                continue
+
+            global_off_scores[token] += expl_off[i].item()
+            global_off_counts[token] += 1
+
+            global_not_scores[token] += expl_not[i].item()
+            global_not_counts[token] += 1
+
+    # Compute average scores
+    print('Calculating average scores')
+    avg_off_scores = {}
+    for token, total_score in global_off_scores.items():
+        count = global_off_counts[token]
+        if count >= args.min_count:
+            avg_off_scores[token] = total_score / count
+
+    # Calculate average "NOT" scores
+    avg_not_scores = {}
+    for token, total_score in global_not_scores.items():
+        count = global_not_counts[token]
+        if count >= args.min_count:
+            avg_not_scores[token] = total_score / count
+
+    # Sort to get Top N
+    sorted_off = sorted(
+        avg_off_scores.items(),
+        key=lambda x: x[1], reverse=True,
+    )
+    sorted_not = sorted(
+        avg_not_scores.items(),
+        key=lambda x: x[1], reverse=True,
+    )
+
+    print('\n' + '='*62)
+    print(f'GLOBAL FEATURE IMPORTANCE (Min. Count: {args.min_count})')
+    print('='*62 + '\n')
+
+    print(f"{'Top N for OFF (Class 0)':<30} | {'Top N for NOT (Class 1)':<30}")
+    print(f"{'-'*28:<30} | {'-'*28:<30}")
+
+    for i in range(args.n):
+        # Get OFF token/score
+        try:
+            tok_off, score_off = sorted_off[i]
+            off_str = f'{i+1: >2}. {tok_off:<15} ({score_off:.4f})'
+        except IndexError:
+            off_str = ''
+
+        # Get NOT token/score
+        try:
+            tok_not, score_not = sorted_not[i]
+            not_str = f'{i+1: >2}. {tok_not:<15} ({score_not:.4f})'
+        except IndexError:
+            not_str = ''
+
+        print(f'{off_str:<30} | {not_str:<30}')
 
     return 0
 
